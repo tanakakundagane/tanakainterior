0580b561a9fec4a283913d368d75bae803e5bd93
Yosuke Iwata <s20970059@nucba.ac.jp>
2025-03-04 23:29:50 +0900

    使用していない変数の削除

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -2,7 +2,6 @@
 
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 
 const images = [
