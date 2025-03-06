d223aaff16bfcf01689bd6f6fb3d92d30a279d6e
Yosuke Iwata <s20970059@nucba.ac.jp>
2025-03-06 23:07:25 +0900

    サービスページの変更

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { FileText } from "lucide-react";
